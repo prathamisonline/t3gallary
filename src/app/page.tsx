e38f9991e03ec79abe6634486065252fe9d8a344
prathamisonline <prathamsrivastavas@gmail.com>
2024-08-18 01:50:09 +0530

    database changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,16 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log("🚀 ~ HomePage ~ posts:", posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
           <div key={image.id + "-" + index} className="w-48">
             <img className="h-44 w-44 object-cover" src={image.url} />
